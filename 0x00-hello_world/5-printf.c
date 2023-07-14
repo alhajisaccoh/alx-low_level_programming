727ce438db561bfa76ccf4bb0f5e7b07ccc5431b
alhajisaccoh <assaccoh@gmail.com>
2023-07-14 08:38:12 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <sdtio.h>
+/**
+ * main - A program that can print a line using the printf function
+ * return: 0 (success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
