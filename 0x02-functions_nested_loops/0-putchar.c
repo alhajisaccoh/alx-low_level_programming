383e65f738d5216312b5cce24e4d48e0df8b02e7
alhajisaccoh <assaccoh@gmail.com>
2023-07-19 01:57:17 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+
+putchar('_')
+putchar('p')
+putchar('u')
+putchar('t')
+putchar('c')
+putchar('h')
+putchar('n')
+putchar('r')
+putchar('\n');
+return (0);
+}
