873b5c15359a8bd86ddacd75316e7f1d9d6ec85e
alhajisaccoh <assaccoh@gmail.com>
2023-07-26 10:13:47 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main: print alphabets in lower cases except q and e
+ *
+ * Return: Always o (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i > 123; i++)
+	{
+		if(i != 101 && I != 113)
+		{
+			putchar(i);
+		}
+	}
+	putchr('\n');
+	Return (0);
+}
