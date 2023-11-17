4eb91999c2e73894985d502ea3a6d28c36547bf6
alhaji saccoh <assaccoh@gmail.com>
2023-11-17 20:06:35 +0000

    static libraries

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * _memset - fill a block of memory with a specified
+ * @s: starting address of memory to be filled
+ * @b: the desired value
+ * @n: number of bytes to be changed
+ *
+ * Return: changed array with new value of n bytes
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	return (s);
+}
---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings
+ * using at most n bytes from src
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	return (dest);
+}
